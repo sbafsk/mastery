461f057f3087e2de60376929ffcdf87fecc37735
sbafsk <razor1836@gmail.com>
2024-01-17 17:37:34 -0300

    add test for functional layer

--- test/quiz_test.exs
+++ test/quiz_test.exs
@@ -2,6 +2,48 @@ defmodule QuizTest do
   use ExUnit.Case
   use QuizBuilders
 
+  describe "when a quiz has two templates" do
+    setup [:quiz]
+
+    test "the next question is randomly selected", %{quiz: quiz} do
+      %{current_question: %{template: first_template}} = Quiz.select_question(quiz)
+
+      other_template = eventually_pick_other_template(quiz, first_template)
+      assert first_template != other_template
+    end
+
+    test "templates are unique until cycle repeats", %{quiz: quiz} do
+      first_quiz = Quiz.select_question(quiz)
+      second_quiz = Quiz.select_question(first_quiz)
+      reset_quiz = Quiz.select_question(second_quiz)
+
+      assert template(first_quiz) != template(second_quiz)
+      assert template(reset_quiz) in [template(first_quiz), template(second_quiz)]
+    end
+  end
+
+  describe "a quiz that always adds one and two" do
+    setup [:quiz_always_adds_one_and_two]
+
+    test "a wrong answer resets mastery", %{quiz: quiz} do
+      quiz
+      |> Quiz.select_question()
+      |> assert_more_questions()
+      |> right_answer()
+      |> Quiz.select_question()
+      |> assert_more_questions()
+      |> wrong_answer()
+      |> Quiz.select_question()
+      |> assert_more_questions()
+      |> right_answer()
+      |> Quiz.select_question()
+      |> assert_more_questions()
+      |> right_answer()
+      |> Quiz.select_question()
+      |> refute_more_questions()
+    end
+  end
+
   defp eventually_pick_other_template(quiz, template) do
     Stream.repeatedly(fn ->
       Quiz.select_question(quiz).current_question.template
--- test/response_test.exs
+++ test/response_test.exs
@@ -2,6 +2,14 @@ defmodule ResponseTest do
   use ExUnit.Case
   use QuizBuilders
 
+  describe "a right response and a wrong response" do
+    setup [:right, :wrong]
+
+    test "building responses check answers", %{right: right, wrong: wrong} do
+      assert right.correct
+      refute wrong.correct
+    end
+  end
 
   defp quiz() do
     fields = template_fields(generators: %{left: [1], right: [2]})
